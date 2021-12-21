613d0155ed183b70c3ea20007fd25025b1a42cb3
Daniel <43097521+Hpro1@users.noreply.github.com>
2021-12-21 09:00:07 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//Select UI Structure Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+//Set initial State of menu
+let showMenu = false;
+
+//When clicking menu btn
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show')); //Adds the show class to the nav items
+    
+        //Set senu state
+        showMenu = true;
+    } 
+    else { //removes the show class from the nav items
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+    
+        //Set Menu State
+        showMenu = false;
+    }
+}
